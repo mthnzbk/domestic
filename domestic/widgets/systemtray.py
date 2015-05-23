91337bc34ece356ab9fac3a2ae3068ad843c94ac
metehan <mthnzbk@gmail.com>
2015-05-23 09:50:42 +0300

    düzeltme.

--- domestic/widgets/systemtray.py
+++ domestic/widgets/systemtray.py
@@ -6,18 +6,13 @@ class SystemTray(QSystemTrayIcon):
     def __init__(self, parent):
         super().__init__()
         self.parent = parent
-        self.menu = FileMenu()
-        self.setContextMenu(self.menu)
         self.setVisible(True)
         self.setIcon(QIcon(":/images/rss-icon-128.png"))
 
         self.updateToolTip()
 
         self.activated.connect(self.parentShow)
-        self.messageClicked.connect(self.messageClick)
-
-    def messageClick(self):
-        self.parent.showNormal()
+        self.messageClicked.connect(self.parentShow)
 
     def updateToolTip(self):
         db = ReaderDb()
@@ -28,10 +23,10 @@ class SystemTray(QSystemTrayIcon):
         db.execute("select * from store where istrash=1")
         trash = db.cursor.fetchall()
         self.setToolTip(self.tr('''<span style='font-size:14pt'>{} - {}</span>
-        <br/><span style='font-size:10pt'>Unread: {}</span>
-        <br/><span style='font-size:10pt'>Stored: {}</span>
-        <br/><span style='font-size:10pt'>Deleted: {}</span>''').format(QApplication.applicationName(),
+        <br><span style='font-size:10pt'>Unread: {}</span>
+        <br><span style='font-size:10pt'>Stored: {}</span>
+        <br><span style='font-size:10pt'>Deleted: {}</span>''').format(QApplication.applicationName(),
         QApplication.applicationVersion(), len(unread), len(store), len(trash)))
 
     def parentShow(self):
-        self.parent.showNormal()
+        self.parent.show()
