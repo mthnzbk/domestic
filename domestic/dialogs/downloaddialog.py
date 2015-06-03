0208bb8c01203e9b1f6b145ee432921535d8ed79
metehan <mthnzbk@gmail.com>
2015-06-03 21:02:30 +0300

    dil desteği eksikliği giderildi. indirme hızı eklendi.

--- domestic/dialogs/downloaddialog.py
+++ domestic/dialogs/downloaddialog.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtCore import QUrl, Qt, QFile, QIODevice, QFileInfo, QDir
 from domestic.core.settings import Settings
 import os.path as os
+import time
 
 class CustomListItem(QWidget):
     def __init__(self, parent=None):
@@ -13,6 +14,9 @@ class CustomListItem(QWidget):
         self.iconLabel.setPixmap(pixmap)
         self.nameLabel = QLabel()
         self.nameLabel.setStyleSheet("font-size:17px; font-weight:bold")
+        self.speedLabel = QLabel()
+        self.speedLabel.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.speedLabel.setStyleSheet("font-size:13px;")
         self.statusLabel = QLabel()
         self.statusLabel.setStyleSheet("font-size:14px; color:green")
         self.progressBar = QProgressBar()
@@ -21,24 +25,30 @@ class CustomListItem(QWidget):
         self.vLayout = QVBoxLayout()
         self.hLayout = QHBoxLayout()
 
-        self.vLayout.addWidget(self.nameLabel)
-
         self.hLayout2 = QHBoxLayout()
-        self.hLayout2.addWidget(self.statusLabel)
-        self.hLayout2.addWidget(self.progressBar)
+        self.hLayout2.addWidget(self.nameLabel)
+        self.hLayout2.addWidget(self.speedLabel)
 
         self.vLayout.addLayout(self.hLayout2)
+
+        self.hLayout3 = QHBoxLayout()
+        self.hLayout3.addWidget(self.statusLabel)
+        self.hLayout3.addWidget(self.progressBar)
+
+        self.vLayout.addLayout(self.hLayout3)
 
         self.hLayout.addWidget(self.iconLabel, 0)
         self.hLayout.addLayout(self.vLayout, 1)
 
         self.setLayout(self.hLayout)
-
         self.manager = QNetworkAccessManager(self)
+
+        self.Time = time.time()
+        self.Value = 0
 
     def startDownload(self, qurl):
         self.url = qurl
-        fileInfo = QFileInfo(qurl.path())
+        fileInfo = QFileInfo(self.url.path())
         fileName = fileInfo.fileName()
         filePath = os.join(QDir.homePath(), fileName)
 
@@ -49,7 +59,7 @@ class CustomListItem(QWidget):
         self.audioFile.open(QIODevice.WriteOnly)
 
         self.nameLabel.setText(fileName)
-        self.statusLabel.setText("İndiriliyor...")
+        self.statusLabel.setText(self.tr("Downloading..."))
 
         self.request = QNetworkRequest(qurl)
         self.request.setRawHeader("User-Agent", "Domestic Browser 1.0")
@@ -65,6 +75,15 @@ class CustomListItem(QWidget):
     def setProgress(self, value, max):
         self.progressBar.setMaximum(max)
         self.progressBar.setValue(value)
+        self.elapsed_time(value, max)
+        self.Time = time.time()
+        self.Value = value
+
+    def elapsed_time(self, value, max):
+        ptime = time.time() - self.Time
+        length = int(value - self.Value)
+        speed = (length // ptime) // 1024 # KB
+        self.speedLabel.setText("{} kb - {} kb".format(str(speed), max//1024))
 
     def finishDownload(self):
         redirectionTarget = self.reply.attribute(QNetworkRequest.RedirectionTargetAttribute)
@@ -79,7 +98,7 @@ class CustomListItem(QWidget):
         else:
             self.audioFile.flush()
             self.audioFile.close()
-            self.statusLabel.setText("İndirildi.")
+            self.statusLabel.setText(self.tr("Downloaded."))
 
 class DownloaderDialog(QDialog):
     def __init__(self, parent=None):
